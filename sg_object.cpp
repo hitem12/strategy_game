ac12aed0c27bf3fe6b75b322cc96320752c4e752
Tomasz Pniewski-Przygodzki <tomaszpnpr@gmail.com>
2018-06-04 21:37:56 +0200

    First Prototype

---
+++ sg_object.cpp
@@ -0,0 +1,28 @@
+#include "sg_unit.h"
+#include "sg_object.h"
+
+//TODO to sie nie powinno zawsze wykonywac
+
+sg_object::sg_object(float x, float y )
+{
+	if (texture.loadFromFile("graphics\\sprites\\cannon\\0000.png") == false) cout << "dont recognize texture\n";
+	sprite.setTexture(texture);
+	sprite.setPosition(x, y);
+	
+}
+
+sg_object::sg_object(sf::Vector2f xy)
+{
+	if (texture.loadFromFile("graphics\\sprites\\cannon\\0001.png") == false) cout << "dont recognize texture\n";
+	sprite.setTexture(texture);
+	sprite.setPosition(xy);
+}
+
+
+sg_object::~sg_object()
+{
+}
+sf::Sprite sg_object::getSprite()
+{
+	return sprite;
+}
